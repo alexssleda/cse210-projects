e7993505a2190569d9f5e5bfa7058a7352858f6b
alexssleda <alexleda2014@gmail.com>
2025-11-07 21:22:44 -0300

    Resumes exercise

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,40 @@
+using System.IO;
+
+public class Journal
+{
+    public String _fileName = "";
+    public List<String> _lines = new List<String>();
+
+    public void SaveFile()
+    {
+
+        Console.WriteLine("What is the file name?");
+        _fileName = Console.ReadLine();
+        Console.WriteLine(_fileName);
+        if (!_fileName.EndsWith(".txt"))
+        {
+            _fileName += ".txt";
+        }
+        using (StreamWriter OutputFile = new StreamWriter(_fileName))
+        {
+            foreach (String line in _lines)
+            {
+                OutputFile.WriteLine(line);
+            }
+        }
+
+    }
+
+    public void LoadFile()
+    {
+        Console.WriteLine("What is the file name?");
+        _fileName = Console.ReadLine();
+        _lines.Clear();
+        string[] lines = System.IO.File.ReadAllLines(_fileName);
+
+        foreach (String line in lines)
+        {
+            _lines.Add(line);
+        }
+    }
+}
